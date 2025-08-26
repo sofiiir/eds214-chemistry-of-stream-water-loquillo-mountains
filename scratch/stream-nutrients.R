ec26a2554347be6ae0afe767fccbb01f93372655
sofiiir <sofiiirodas@gmail.com>
2025-08-25 23:59:47 -0700

    finishing up rollmean

--- scratch/stream-nutrients.R
+++ scratch/stream-nutrients.R
@@ -18,6 +18,7 @@ bq3_data <- read_csv(here::here("data", "QuebradaCuenca3-Bisley.csv"))
 
 prm_data <- read_csv(here::here("data", "RioMameyesPuenteRoto.csv"))
 
+# checking the class of dates to make sure lubricate isn't necessary in this scenario
 class(bq1_data$sample_date)
 
 #clean data
@@ -29,7 +30,7 @@ prm_data <- prm_data |> clean_names()
 
 
 
-# filter
+# filter for the data columns needed in the time frame needed
 bq1 <- bq1_data |> 
   select(sample_id, sample_date, k, na, mg, ca, nh4_n) |> 
   filter(sample_date >= "1988-01-05",
@@ -51,22 +52,27 @@ prm <- prm_data |>
   filter(sample_date >= "1988-01-05",
          sample_date <= "1994-12-27")
 
-# add lubridate day 
+# add lubridate day column for analysis
+bq1 <- bq1 |> mutate(week = week(sample_date))
 
 #join data
+# not joining data anymore with the potential to use patchwork to piece the different graphs together, may change approach later
 #data <- bq1 |> full_join(bq2, by = "sample_date")
 
 
 #calculate 9 day moving average
 #time_series <-  bq1 |> ts()
 bq1_rma <- bq1 |> 
-  mutate(rmak = rollmean())
+  mutate(kcma = rollmean(k, k = 2, fill = NA))
+#this gets the moving average across the 2 values around the variable and only works for 
 
 
-# ggplot
-ggplot(data = bq1, aes(x = sample_date, y = k)) +
+# ggplot of the rollmean average 
+ggplot(data = bq1_rma, aes(x = sample_date, y = kcma)) +
   geom_line() +
   labs(x = "Years",
        y = "K mg |-1")
 
 #patchwork
+
+
